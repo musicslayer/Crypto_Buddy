2c0f11d0c6ae1283a70165869ad1cb8e5325d5b3
musicslayer <musicslayer@gmail.com>
2022-03-06 13:23:06 -0500

    Remove price from ExchangeTable Update help to clarify we break up buys/sells.

--- app/src/main/java/com/musicslayer/cryptobuddy/view/table/ExchangeTable.java
+++ app/src/main/java/com/musicslayer/cryptobuddy/view/table/ExchangeTable.java
@@ -9,22 +9,13 @@ import androidx.appcompat.widget.AppCompatTextView;
 import com.musicslayer.cryptobuddy.R;
 import com.musicslayer.cryptobuddy.api.exchange.ExchangeData;
 import com.musicslayer.cryptobuddy.rich.RichStringBuilder;
-import com.musicslayer.cryptobuddy.settings.setting.PriceDisplaySetting;
 import com.musicslayer.cryptobuddy.transaction.Transaction;
 
 import java.util.ArrayList;
 
 public class ExchangeTable extends Table {
     public BaseRow getRow(Transaction transaction) {
-        return new ExchangeRow(getContext(), transaction);
-    }
-
-    public static boolean shouldAddForwardsPrice() {
-        return "Forward".equals(PriceDisplaySetting.value) || "ForwardBackward".equals(PriceDisplaySetting.value);
-    }
-
-    public static boolean shouldAddBackwardsPrice() {
-        return "ForwardBackward".equals(PriceDisplaySetting.value);
+        return new ExchangeTable.ExchangeRow(getContext(), transaction);
     }
 
     public ExchangeTable(Context context) {
@@ -34,11 +25,8 @@ public class ExchangeTable extends Table {
     public ExchangeTable(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
 
-        this.addColumn("action", "Action", "discrete", 1);
-        this.addColumn("quantity", "Actioned Asset", "discrete", 1);
-        this.addColumn("other_quantity", "Other Asset",  "discrete", 1);
-        if(shouldAddForwardsPrice()) { this.addColumn("price", "Forward Price", "discrete", 1); }
-        if(shouldAddBackwardsPrice()) { this.addColumn("other_price", "Backward Price", "discrete", 1); }
+        this.addColumn("action", "Action","discrete", 1);
+        this.addColumn("quantity", "Asset", "discrete", 1);
         this.addColumn("timestamp", "Timestamp", "date", 0);
         this.addColumn("info", "Info", "discrete", 1);
 
@@ -86,35 +74,17 @@ public class ExchangeTable extends Table {
             t1.setText(Html.fromHtml(s1.toString()));
 
             AppCompatTextView t2 = new AppCompatTextView(context);
+            t2.setText(transaction.timestamp.toString());
             t2.setBackgroundResource(R.drawable.border);
 
-            RichStringBuilder s2 = new RichStringBuilder(true);
-            s2.appendAssetQuantity(transaction.otherAssetQuantity);
-            t2.setText(Html.fromHtml(s2.toString()));
-
             AppCompatTextView t3 = new AppCompatTextView(context);
-            t3.setText(transaction.forwardPrice.toString());
+            t3.setText(transaction.info);
             t3.setBackgroundResource(R.drawable.border);
-
-            AppCompatTextView t4 = new AppCompatTextView(context);
-            t4.setText(transaction.backwardPrice.toString());
-            t4.setBackgroundResource(R.drawable.border);
-
-            AppCompatTextView t5 = new AppCompatTextView(context);
-            t5.setText(transaction.timestamp.toString());
-            t5.setBackgroundResource(R.drawable.border);
-
-            AppCompatTextView t6 = new AppCompatTextView(context);
-            t6.setText(transaction.info);
-            t6.setBackgroundResource(R.drawable.border);
 
             this.addView(t0);
             this.addView(t1);
             this.addView(t2);
-            if(shouldAddForwardsPrice()){this.addView(t3);}
-            if(shouldAddBackwardsPrice()){this.addView(t4);}
-            this.addView(t5);
-            this.addView(t6);
+            this.addView(t3);
         }
     }
 }
